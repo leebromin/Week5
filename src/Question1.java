da7284622c75c3c37b98c4020eef6aac71ab7983
이형민 <mini100142@gmail.com>
2024-10-02 16:48:48 +0900

    20241002

---
+++ src/Question1.java
@@ -0,0 +1,11 @@
+public class Question1 {
+    public static void main(String[] args) {
+        int n = 7;
+
+        System.out.println(n + "단:");
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
+
